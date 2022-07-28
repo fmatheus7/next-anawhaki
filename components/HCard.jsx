947b90714abe5d66a039c88a440c613220e5ec36
Matheus Freire <matheusfreire77@gmail.com>
2022-07-28 10:14:23 +0100

    Update HCard.jsx

--- components/HCard.jsx
+++ components/HCard.jsx
@@ -6,13 +6,7 @@ const HCard = () => {
     <div className='my-4 mx-4'>
       <div className='flex justify-center space-y-4'>
         <div className='rounded-lg shadow-lg bg-white max-w-sm'>
-          <a href='#!' data-mdb-ripple='true' data-mdb-ripple-color='light'>
-            <img
-              className='rounded-t-lg'
-              src='https://mdbootstrap.com/img/new/standard/nature/182.jpg'
-              alt=''
-            />
-          </a>
+          <a href='#!' data-mdb-ripple='true' data-mdb-ripple-color='light'></a>
           <div className='p-6 flex items-center justify-between'>
             <h5 className='text-gray-900 text-xl font-medium mb-2 font-bold '>
               Ayahuasca
